5034e510acafbc14a6ccc3bc9d7ff979d6924159
191GPZ2016A <zakjimenez@outlook.com>
2017-03-21 23:13:29 +0800

    Add sprint 4 files

--- 05-Implementation (Software)/wifinder/views.py
+++ 05-Implementation (Software)/wifinder/views.py
@@ -29,29 +29,46 @@ def results(request):
         )
         res = c.execute("""
             SELECT name, wifiid, AVG(rating), dist.distance AS distance
-            FROM ratings JOIN wifinames ON (ratings.wifiid = wifinames.id) JOIN (
-                SELECT locID, (
-                    6371 * acos(
-                        cos(radians({0})) *
-                        cos(radians(lat)) *
-                        cos(radians(lng) - radians({1})) +
-                        sin(radians({0})) *
-                        sin(radians(lat))
+            FROM
+                (
+                    SELECT *
+                    FROM ratings UNION (
+                        SELECT 0, id, 0, ""
+                        FROM wifinames
+                        WHERE id NOT IN (
+                            SELECT wifiid
+                            FROM ratings
+                        )
                     )
-                ) AS distance
-                FROM locations
-                ORDER BY distance
-                LIMIT 0 , 5
-            ) dist ON (dist.locID = wifinames.location)
+                ) rates
+                JOIN wifinames ON (
+                    rates.wifiid = wifinames.id
+                )
+                JOIN (
+                    SELECT locID, (
+                        6371 * acos(
+                            cos(radians({0})) *
+                            cos(radians(lat)) *
+                            cos(radians(lng) - radians({1})) +
+                            sin(radians({0})) *
+                            sin(radians(lat))
+                        )
+                    ) AS distance
+                    FROM locations
+                    ORDER BY distance
+                    LIMIT 0 , 5
+                ) dist ON (dist.locID = wifinames.location)
             GROUP BY wifiid
             ORDER BY dist.distance
             ;""".format(*(c.fetchall()[0]))
         )
         data = [[row[0],int(round(row[1])),int(round(row[2])),round(row[3]*1000,1)] for row in c.fetchall()]
-        
         c.close()
         
-        return render(request,"wifinder/results.html",{"results":data,"src":request.POST["loc"]})
+        return render(request,"wifinder/results.html",{
+            "results": data,
+            "src": request.POST["loc"]
+        })
 #    except Exception:
 #        return index(request)
 
@@ -71,35 +88,58 @@ def wifi(request,src,dst):
     )
     res = c.execute("""
         SELECT name, wifiid, AVG(rating), dist.distance AS distance, locID
-        FROM ratings JOIN wifinames ON (ratings.wifiid = wifinames.id) JOIN (
-            SELECT locID, (
-                6371 * acos(
-                    cos(radians({1})) *
-                    cos(radians(lat)) *
-                    cos(radians(lng) - radians({2})) +
-                    sin(radians({1})) *
-                    sin(radians(lat))
+        FROM
+            (
+                SELECT *
+                FROM ratings UNION (
+                    SELECT 0, id, 0, ""
+                    FROM wifinames
+                    WHERE id NOT IN (
+                        SELECT wifiid
+                        FROM ratings
+                    )
                 )
-            ) AS distance
-            FROM locations
-        ) dist ON (dist.locID = wifinames.location)
+            ) rates
+            JOIN wifinames ON (
+                rates.wifiid = wifinames.id
+            )
+            JOIN (
+                SELECT locID, (
+                    6371 * acos(
+                        cos(radians({1})) *
+                        cos(radians(lat)) *
+                        cos(radians(lng) - radians({2})) +
+                        sin(radians({1})) *
+                        sin(radians(lat))
+                    )
+                ) AS distance
+                FROM locations
+            ) dist ON (
+                dist.locID = wifinames.location
+            )
         WHERE wifiid = {0}
         ;""".format(dst,*(c.fetchall()[0]))
     )
     data = [[row[0],int(round(row[1])),int(round(row[2])),round(row[3]*1000,1),int(row[4])] for row in c.fetchall()][0]
-    
+    res = c.execute("""
+        SELECT rating, comment
+        FROM ratings
+        WHERE wifiid = {}
+        """.format(dst)
+    )
+    revs = [{"rate":int(row[0]),"comment":row[1]} for row in c.fetchall()]
+    print "revs",revs;
     c.close()
-    print data;
     
-    return render(request,"wifinder/wifi.html",{"wifi":data})
+    return render(request,"wifinder/wifi.html",{"wifi":data,"reviews":revs})
 
 def thanks(request):
     if "rate" in request.POST.keys():        db = MySQLdb.connect(user="clientlogin",passwd="clientw3w",db="wifinder192")
         c = db.cursor()
         c.execute("""
-            INSERT INTO ratings(wifiid,rating)
-            VALUES({},{})
-            ;COMMIT;""".format(*(request.POST[key] for key in ("wifiid","rate")))
+            INSERT INTO ratings(wifiid,rating,comment)
+            VALUES({},{},"{}")
+            ;COMMIT;""".format(*(request.POST[key] for key in ("wifiid","rate","comment")))
         )
         c.close()
     return render(request,"wifinder/thanks.html",{"request":request.POST})
